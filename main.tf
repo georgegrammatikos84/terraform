a41f773883f2462ac4707dac332a076edc35cf0c
George <george@cloudopszonecom.onmicrosoft.com>
2023-05-11 19:09:49 +0300

    Update main

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.55.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 }
@@ -19,95 +9,10 @@ name = "gblazure23-tf-rg"
 location = "westeurope"
 
 }
-resource "azurerm_virtual_network" "main" {
-  name                = "gblazure23-tf-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+
+output "id" {
+	value= data.azurerm_resource_group.main.id
 }
-
-resource "azurerm_subnet" "main" {
-  name                 = "gblazure23-tf-subnet"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_security_group" "main" {
-  name                = "gblaz23nsg"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  security_rule {
-    name                       = "test123"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-}
-
-resource "azurerm_network_interface" "main" {
-  name                = "gblazure23-tf-nic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.main.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "main" {
-  network_interface_id      = azurerm_network_interface.main.id
-  network_security_group_id = azurerm_network_security_group.main.id
-}
-
-#Create windows virtual machine
-
-resource "azurerm_windows_virtual_machine" "main" {
-  name = "gblaz23-tf-vm"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  size                = "Standard_B1s"
-  admin_username      = "user.ops"
-  admin_password      = "P@$$w0rd1234!"
-  network_interface_ids = [
-    azurerm_network_interface.main.id
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
-    version   = "latest"
-  }
-}
-
-resource "azurerm_managed_disk" "main" {
-  name                 = "disk-westeurope"
-  location             = azurerm_resource_group.main.location
-  resource_group_name  = azurerm_resource_group.main.name
-  storage_account_type = "Standard_LRS"
-  create_option        = "Empty"
-  disk_size_gb         = "1024"
-
-  tags = {
-    environment = "staging"
-  }
-}
-
 
 
 
